1724a61546ac68b1afafb3d717a40fbced14a5b9
Mikhail <44231064+inf1111@users.noreply.github.com>
2025-03-05 14:00:03 +0300

    убрал все наложения

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,21 @@ Route::get('/', [MainController::class, 'showIndex'])->name('show-index');
 Route::get('/showProduct/{product_id}', [MainController::class, 'showProduct'])->name('show-product');
 
 Route::middleware('guest')->group(function () {
+
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+
     Route::post('/login', [AuthController::class, 'login']);
+
 });
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
     Route::delete('/cart/remove-item', [CartTable::class, 'deleteItem'])->name('cart.remove-item');
+
     Route::get('/cart', [MainController::class, 'showCart'])->name('show-cart');
 
     Route::get('/checkout', [MainController::class, 'showCheckout'])->name('show-checkout'); // Страница с формой
@@ -30,6 +37,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/payment/success', [MainController::class, 'showSuccess'])->name('show-success'); // Успешный платеж
 
     Route::get('/payment/failed', [MainController::class, 'showFailed'])->name('show-failed'); // Ошибка
+
 });
 
 
